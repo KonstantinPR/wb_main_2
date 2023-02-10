f13998f89fe4edac9e70888e48bb7af0d01ae8d1
Costa <konstantinpr@ya.ru>
2023-02-11 01:52:04 +0300

    requirements

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:19862814@localhost:8000/wb_main'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 
 @app.route('/')
 def index():
     return 'Hello, World!'
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
