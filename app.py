678d26d3bad810fd4a09bdb41e7cf987bd65695c
Costa <konstantinpr@ya.ru>
2023-02-11 17:18:01 +0300

    requirements

--- app.py
+++ app.py
@@ -13,9 +13,9 @@ uri_old = os.getenv("DATABASE_URL")  # or other relevant config var
 uri = os.environ.get('DATABASE_URL')
 print(f"uri in __init__ {uri}")
 
-if uri:
-    if uri.startswith("postgres://"):
-        uri = uri.replace("postgres://", "postgresql://", 1)
+# if uri:
+#     if uri.startswith("postgres://"):
+#         uri = uri.replace("postgres://", "postgresql://", 1)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = uri or 'postgresql://postgres:19862814@localhost:8000/wb_main'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
